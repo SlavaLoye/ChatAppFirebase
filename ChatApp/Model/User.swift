f5c86fef89bb0297f3851ac827696506a3573c32
SlavaLoye <electromusiccompany@gmail.com>
2018-08-11 19:08:10 +0300

    add[NewProject]

---
+++ ChatApp/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by Вячеслав Лойе on 11.08.2018.
+//  Copyright © 2018 Вячеслав Лойе. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var id: String?
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    init(dictionary: [String: AnyObject]) {
+        self.id = dictionary["id"] as? String
+        self.name = dictionary["name"] as? String
+        self.email = dictionary["email"] as? String
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+    }
+}
+
